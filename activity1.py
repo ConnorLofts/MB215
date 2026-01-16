e83400810a327398514ddc376ba1b1d837ceb59f
ConnorLofts <loft7450@mylaurier.ca>
2026-01-16 18:57:39 -0500

    Update activity1.py

--- activity1.py
+++ activity1.py
@@ -1,13 +1,15 @@
 # Activity 1
 import datetime
 
-# Write a program that asks for the user's name and age.
+# Asking for user's for their name and age.
 name = input("Please enter your name")
 age = input("Please enter your age")
 
-# Print a greeting and calculate the year the user was born
+# Calculating the year the user was born
 age = int(age)
 current_year = datetime.datetime.now().year
 birth_year = current_year - age
+
+# Printing a greeting and calculate the year the user was born
 print(f"You were born in {birth_year}")
 print(f"Greetings {name}")
