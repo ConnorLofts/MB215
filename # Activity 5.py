c64b801af86dccd0fdd3ebfe24369bbf8a97e7f4
ConnorLofts <loft7450@mylaurier.ca>
2026-01-21 15:51:08 -0500

    Delete # Activity 5.py

--- # Activity 5.py
+++
@@ -1,9 +0,0 @@
-# Activity 5
-# The calculation of the area of a circle
-pi = 3.1415959
-radius = float(input("Enter the radius of the circle: "))
-area = pi * radius ** 2
-# the output
-print("The area of the circle is:", area)
-
-
