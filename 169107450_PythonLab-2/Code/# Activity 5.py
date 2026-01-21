a16d35f4b72db8df4a5373b41c1818b33e2e9284
ConnorLofts <loft7450@mylaurier.ca>
2026-01-21 15:51:29 -0500

    Create # Activity 5.py

---
+++ 169107450_PythonLab-2/Code/# Activity 5.py
@@ -0,0 +1,9 @@
+# Activity 5
+# The calculation of the area of a circle
+pi = 3.1415959
+radius = float(input("Enter the radius of the circle: "))
+area = pi * radius ** 2
+# the output
+print("The area of the circle is:", area)
+
+
