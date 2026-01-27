41c7208e015cbc19005b5941173c9be9fbebb1b4
ConnorLofts <loft7450@mylaurier.ca>
2026-01-27 16:40:08 -0500

    Create # Activity 1.py

---
+++ 169107450_PythonLab-3/Code/# Activity 1.py
@@ -0,0 +1,9 @@
+# Activity 1
+Km = float(input("Enter distance in kilometers: "))
+# Converting kilometers to miles
+Miles = Km * (1/1.609)
+# Rounding to 2 decimal places
+Round_miles = round(Miles, 2)
+# Displaying the result
+print(f"{Km} kilometers is equal to {Round_miles} miles.")
+
