bd69782b14dbde03ed49fedee088726de1186977
ConnorLofts <loft7450@mylaurier.ca>
2026-01-27 16:54:52 -0500

    Update # Activity 5.py

--- 169107450_PythonLab-3/Code/# Activity 5.py
+++ 169107450_PythonLab-3/Code/# Activity 5.py
@@ -1,12 +1,11 @@
 # Activity 5
-import keyboard
 Number_added = int(input("Enter a number to add to your sum: "))
 Sum = 0
 while Sum < 100:
     Sum = Sum + Number_added
     if Sum < 100:
         Number_added = int(input(f"Enter another number to add to your sum. Your current sum is {Sum}: "))
-    elif keyboard.is_Pressed ("space"):
+    elif Number_added == "":
         break
 # Displaying the result 
 print(f"The total sum is: {Sum}")
