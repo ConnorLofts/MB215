9e2cd3e6eabe65d356e1dda14dc99059db4f3483
ConnorLofts <loft7450@mylaurier.ca>
2026-01-31 14:16:04 -0500

    Delete # Activity 1.py

--- 169107450_PythonLab-4/Code/# Activity 1.py
+++
@@ -1,8 +0,0 @@
-# Activity 1: Customizable Dice Simulator
-import random
-
-def roll_dice(num_dice, num_sides):
-    total = 0
-    for _ in range(num_dice):
-        total += random.randint(1, num_sides)
-    return total
