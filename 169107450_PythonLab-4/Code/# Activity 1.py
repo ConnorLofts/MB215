c18e454745911bba46521644bf51b805b15865b4
ConnorLofts <loft7450@mylaurier.ca>
2026-01-30 20:20:43 -0500

    Update # Activity 1.py

--- 169107450_PythonLab-4/Code/# Activity 1.py
+++ 169107450_PythonLab-4/Code/# Activity 1.py
@@ -3,9 +3,6 @@ import random
 
 def roll_dice(num_dice, num_sides):
     total = 0
-     
     for _ in range(num_dice):
-        roll = random.randint(1, num_sides)
-        total += roll
-
+        total += random.randint(1, num_sides)
     return total
