a64519fe03f7a5c255a7bbe514c1acdfe0525593
ConnorLofts <loft7450@mylaurier.ca>
2026-02-05 11:11:51 -0500

    Create # Activity 6.py

---
+++ 169107450_PythonLab-3/Code/# Activity 6.py
@@ -0,0 +1,22 @@
+# Activity 6
+
+# For loop with range to print even numbers from 2 to 20
+print("Even numbers from 2 to 20:")
+for num in range(2, 21, 2):
+    print(num, end=' ')
+print("\n")
+
+# Nested for loop to create a multiplication table for numbers 1 to 3
+print("Multiplication table for numbers 1 to 3:")
+for i in range(1, 4):  
+    for j in range(1, 11):  
+        print(f"{i} * {j} = {i * j}", end='\t')
+    print()  
+print()
+
+# For loop to reverse a string. Note: Use ‘reversed’ keyword
+given_string = "Hello"
+print(f"Reversing the string '{given_string}':")
+for char in reversed(given_string):
+    print(char, end=' ')
+print()
