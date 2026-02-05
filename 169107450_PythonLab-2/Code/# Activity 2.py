956cb62555e5b88fa57bf57063848f8de45ac33d
ConnorLofts <loft7450@mylaurier.ca>
2026-02-05 11:10:50 -0500

    Create # Activity 2.py

---
+++ 169107450_PythonLab-2/Code/# Activity 2.py
@@ -0,0 +1,8 @@
+# Activity 2
+# Getting inputs for length and width of a rectangle
+length = float(input("Please enter the length"))
+width = float(input("Please enter the width"))  
+# Calculating the area of the rectangle
+area = length * width
+# Printing the area
+print(f"The area of the rectangle is {area}")
