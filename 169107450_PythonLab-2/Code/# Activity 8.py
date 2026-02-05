f92c34462b595c03c22d0b591146ef0208c2e2aa
ConnorLofts <loft7450@mylaurier.ca>
2026-02-05 11:11:06 -0500

    Create # Activity 8.py

---
+++ 169107450_PythonLab-2/Code/# Activity 8.py
@@ -0,0 +1,7 @@
+# Activity 8
+# Get inputs for hours worked and hourly rate
+hour_work = float(input("Enter number of hours worked: "))
+hourly_rate = float(input("Enter hourly rate: "))
+# Calculate gross pay
+gross_pay = hour_work * hourly_rate
+print("Gross Pay: $", gross_pay)
