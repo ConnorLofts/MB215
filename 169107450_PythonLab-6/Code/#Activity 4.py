281528f39dd06cf7e178b9a66035d39e1f24bbaa
ConnorLofts <loft7450@mylaurier.ca>
2026-02-13 12:44:46 -0500

    Update #Activity 4.py

--- 169107450_PythonLab-6/Code/#Activity 4.py
+++ 169107450_PythonLab-6/Code/#Activity 4.py
@@ -1,14 +1,18 @@
-##Activity 4
-list1 = [1, 2, 3]
-list2 = [4, 5, 6]
-concatenated_list = list1 + list2
-print("Concatenated list using + operator:", concatenated_list)
+#Activity 4: The __str__ Method
+#Implement the __str__ method to define a string representation for the #Car class instances.
+class Car:
+    # Existing methods and __init__# car.py (updated)
+    def __init__(self, make, model, year):
+        self.make = make
+        self._model = model
+        self.year = year
 
-list1 = [1, 2, 3]
-list2 = [4, 5, 6]
-list1 += list2
-print("Concatenated list using += operator:", list1)
+    def display_info(self):
+         print(f"Car Details: {self.year} {self.make} {self.model}")
 
+    def __update_model(self, new_model):
+        self._model = new_model
 
-list1 = [1, 2, 3]
-number = 4
+    def __str__(self):
+        return f"Car(Make: {self.make}, Model: {self._model}, Year: {self.year})"
+
