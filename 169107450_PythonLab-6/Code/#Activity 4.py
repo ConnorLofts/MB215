c077c6cae75e6bf599832dfe7343bec5c653058d
ConnorLofts <loft7450@mylaurier.ca>
2026-02-13 16:39:36 -0500

    Update #Activity 4.py

--- 169107450_PythonLab-6/Code/#Activity 4.py
+++ 169107450_PythonLab-6/Code/#Activity 4.py
@@ -1,7 +1,7 @@
 #Activity 4: The __str__ Method
 #Implement the __str__ method to define a string representation for the #Car class instances.
 class Car:
-    # Existing methods and __init__# car.py (updated)
+  # Existing methods and __init__
     def __init__(self, make, model, year):
         self.make = make
         self._model = model
@@ -14,5 +14,6 @@ class Car:
         self._model = new_model
 
     def __str__(self):
+        # Your code here to return a string representation
         return f"Car(Make: {self.make}, Model: {self._model}, Year: {self.year})"
 
