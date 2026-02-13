41b7b97733becac9e236b07f5849c2bd90cc36d9
ConnorLofts <loft7450@mylaurier.ca>
2026-02-13 17:26:25 -0500

    Update #Activity 7.py

--- 169107450_PythonLab-6/Code/#Activity 7.py
+++ 169107450_PythonLab-6/Code/#Activity 7.py
@@ -4,11 +4,10 @@ from car import Car
 def compare_car_years(car1, car2):
     return car1.year < car2.year
 
-# Your code here to define with values and print the comparison output
 car1 = Car("Tesla", "Model 3", 2022)
 car2 = Car("Rivian", "R1T", 2024)
 
 if compare_car_years(car1, car2):
-    print(f"{car1} is older than {car2}")
+    print(f"{car1.year} is older than {car2.year}")
 else:
-    print(f"{car1} is not older than {car2}")
+    print(f"{car1.year} is not older than {car2.year}")
