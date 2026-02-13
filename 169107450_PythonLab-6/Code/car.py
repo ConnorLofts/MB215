cc892e657e28cdc27648de9f3035a717b43a07d9
ConnorLofts <loft7450@mylaurier.ca>
2026-02-13 12:44:52 -0500

    Create car.py

---
+++ 169107450_PythonLab-6/Code/car.py
@@ -0,0 +1,9 @@
+##Activity 1: Define a Car Class
+##Create a file named car.py and define a Car class with the following ##specifications:
+class Car:
+    def __init__(self, make, model, year):
+        # Your code here to initialize attributes
+        self.make = make
+        self.model = model
+        self.year = year
+        pass
